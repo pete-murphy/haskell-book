696bf975efce8d1adabd3a9ee03dcc032ddd28a2
Peter Murphy <peterfrancismurphy@gmail.com>
2018-05-20 13:35:19 -0400

    Ch 18.03, p1165:t02

---
+++ src/18/02-WriteBind.hs
@@ -0,0 +1,6 @@
+module WriteBind where
+
+import           Control.Monad (join)
+
+bind' :: Monad m => (a -> m b) -> m a -> m b
+bind' f = join . fmap f
---
+++ src/18/03-WhenFmapAloneIsntEnough.hs
@@ -0,0 +1,26 @@
+module WhenFmapAloneIsntEnough where
+
+bindingAndSequencing :: IO ()
+bindingAndSequencing = do
+  putStrLn "name pls:"
+  name <- getLine
+  putStrLn ("y helo thar: " ++ name)
+
+bindingAndSequencing' :: IO ()
+bindingAndSequencing' =
+  putStrLn "name pls:" >> getLine >>= \name ->
+    putStrLn ("y helo thar: " ++ name)
+
+twoBinds :: IO ()
+twoBinds = do
+  putStrLn "name pls:"
+  name <- getLine
+  putStrLn "age pls:"
+  age <- getLine
+  putStrLn ("y helo thar: " ++ name ++ " who is: " ++ age ++ " years old.")
+
+twoBinds' :: IO ()
+twoBinds' =
+  putStrLn "name pls:" >> getLine >>= \name ->
+    putStrLn "age pls:" >> getLine >>= \age ->
+      putStrLn ("y helo thar: " ++ name ++ " who is " ++ age ++ " years old.")
