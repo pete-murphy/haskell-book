36a383b2668d875c5e5d8b2e83f1332f01510b56
Peter Murphy <peterfrancismurphy@gmail.com>
2018-05-21 23:32:24 -0400

    Ch 18.7, p1208:t02, might be cheating a bit with my Arbitrary instance for List

--- src/18/07-ChapterExercises-1.hs
+++ src/18/07-ChapterExercises-1.hs
@@ -4,6 +4,7 @@ import           Test.QuickCheck
 import           Test.QuickCheck.Checkers
 import           Test.QuickCheck.Classes
 
+-- 1
 data Nope a =
   NopeDotJpg
   deriving (Eq, Show)
@@ -52,9 +53,61 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (PhhhbbtttEither b a) where
 instance (Eq a, Eq b) => EqProp (PhhhbbtttEither b a) where
   (=-=) = eq
 
+-- 3
+newtype Identity a =
+  Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+
+instance Applicative Identity where
+  pure = Identity
+  Identity f <*> Identity x = Identity (f x)
+
+instance Monad Identity where
+  return = pure
+  Identity x >>= f = f x
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = Identity <$> arbitrary
+
+instance Eq a => EqProp (Identity a) where
+  (=-=) = eq
+
+-- 4
+data List a
+  = Nil
+  | Cons a
+         (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil         = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+instance Applicative List where
+  pure x = Cons x Nil
+  Nil <*> _ = Nil
+  _ <*> Nil = Nil
+  Cons f fs <*> Cons x xs = Cons (f x) (fs <*> xs)
+
+instance Monad List where
+  return = pure
+  Nil >>= _ = Nil
+  Cons x _ >>= f = f x
+
+instance Arbitrary a => Arbitrary (List a) where
+  arbitrary = pure <$> arbitrary
+
+instance Eq a => EqProp (List a) where
+  (=-=) = eq
+
 main :: IO ()
 main = do
   quickBatch $ monad (undefined :: Nope (Int, String, [Int]))
   quickBatch $
     monad
       (undefined :: PhhhbbtttEither (Int, String, [Int]) (Int, String, [Int]))
+  quickBatch $ monad (undefined :: Identity (Int, String, [Int]))
+  quickBatch $ monad (undefined :: List (Int, String, [Int]))
