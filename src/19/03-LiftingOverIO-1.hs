b21ef2d53d0ead3dcbdffc5028f886112c52dc2b
Peter Murphy <peterfrancismurphy@gmail.com>
2018-05-23 23:02:29 -0400

    Ch 19.3, p1224:t02

---
+++ src/19/03-LiftingOverIO-1.hs
@@ -0,0 +1,6 @@
+module LiftingOverIO where
+
+import           Data.Time.Clock
+
+offsetCurrentTime :: NominalDiffTime -> IO UTCTime
+offsetCurrentTime offset = fmap (addUTCTime (offset * 24 * 3600)) getCurrentTime
