6bd37c1e19ccb4d36f923162cac25012b5cff5af
Peter Murphy <peterfrancismurphy@gmail.com>
2018-05-23 22:20:14 -0400

    Ch 19.2, p1218:t01

---
+++ src/19/02-TemplatingContentInScotty.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module TemplatingInScotty where
+
+import           Data.Monoid (mconcat)
+import           Web.Scotty
+
+main =
+  scotty 3000 $
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
