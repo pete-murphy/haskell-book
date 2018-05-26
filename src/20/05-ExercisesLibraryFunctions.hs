cff80163866282f296dc713686c06ad81316a71a
Peter Murphy <peterfrancismurphy@gmail.com>
2018-05-26 15:37:13 -0400

    Ch 20.6, p1281:t02. Not fully grasping , need to revisit.

--- src/20/05-ExercisesLibraryFunctions.hs
+++ src/20/05-ExercisesLibraryFunctions.hs
@@ -19,4 +19,34 @@ product'' = foldr (*) 1
 
 -- 3
 elem' :: (Foldable t, Eq a) => a -> t a -> Bool
-elem' n xs = foldr (||) False $ map ((==) n) xs
+elem' n = foldr ((||) . (==) n) False
+
+-- This doesn't work:
+-- elem' n xs = foldr (||) False $ map ((==) n) xs
+-- 4
+minimum' :: (Foldable t, Ord a) => t a -> Maybe a
+minimum' = foldr (min . Just) Nothing
+
+-- 5
+maximum' :: (Foldable t, Ord a) => t a -> Maybe a
+maximum' = foldr (max . Just) Nothing
+
+-- 6
+null' :: (Foldable t) => t a -> Bool
+null' = foldr (\_ _ -> False) True
+
+-- 7
+length' :: (Foldable t) => t a -> Int
+length' = foldr ((+) . const 1) 0
+
+-- 8
+toList' :: (Foldable t) => t a -> [a]
+toList' = foldr (:) []
+
+-- 9
+fold' :: (Foldable t, Monoid m) => t m -> m
+fold' = foldMap id
+
+-- 10
+foldMap' :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+foldMap' f = foldr ((<>) . f) mempty
