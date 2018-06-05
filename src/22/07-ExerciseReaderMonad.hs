ad60183fb3a327c6bcbc7c55bf6e77b17cb388d9
Peter Murphy <peterfrancismurphy@gmail.com>
2018-06-04 23:06:09 -0400

    Ch 22.11, p1357:t03

--- src/22/07-ExerciseReaderMonad.hs
+++ src/22/07-ExerciseReaderMonad.hs
@@ -14,7 +14,33 @@ instance Applicative (Reader r) where
   (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
   Reader rab <*> Reader ra = Reader (rab <*> ra)
 
+-- 1
 instance Monad (Reader r) where
   return = pure
   (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
   Reader ra >>= aRb = Reader ra >>= aRb
+
+-- 2
+data Person = Person
+  { humanName :: String
+  , dogName   :: String
+  , address   :: String
+  } deriving (Eq, Show)
+
+data Dog = Dog
+  { dogsName    :: String
+  , dogsAddress :: String
+  } deriving (Eq, Show)
+
+getDogR :: Person -> Dog
+getDogR = Dog <$> dogName <*> address
+
+getDogRM :: Person -> Dog
+getDogRM = do
+  name <- dogName
+  addy <- address
+  return $ Dog name addy
+
+getDogRM' :: Person -> Dog
+getDogRM' =
+  dogName >>= \name -> address >>= \address -> return $ Dog name address
