ccbc67f17f5627153fe70fce74d752cdd79d001a
Peter Murphy <peterfrancismurphy@gmail.com>
2018-07-09 23:59:52 -0400

    Ch 24.10, p1475:t02

--- src/24/07-DataIni.hs
+++ src/24/07-DataIni.hs
@@ -97,6 +97,15 @@ newtype Config =
 skipWhitespace :: Parser ()
 skipWhitespace = skipMany (char ' ' <|> char '\n')
 
+rollup :: Section -> Map Header Assignments -> Map Header Assignments
+rollup (Section h a) m = M.insert h a m
+
+parseIni :: Parser Config
+parseIni = do
+  sections <- some parseSection
+  let mapOfSections = foldr rollup M.empty sections
+  return (Config mapOfSections)
+
 parseSection :: Parser Section
 parseSection = do
   skipWhitespace
@@ -105,3 +114,55 @@ parseSection = do
   skipEOL
   assignments <- some parseAssignment
   return $ Section h (M.fromList assignments)
+
+maybeSuccess :: Result a -> Maybe a
+maybeSuccess (Success a) = Just a
+maybeSuccess _           = Nothing
+
+main :: IO ()
+main =
+  hspec $ do
+    describe "Assignment Parsing" $
+      it "can parse a simple assignment" $ do
+        let m = parseByteString parseAssignment mempty assignmentEx
+            r' = maybeSuccess m
+        print m
+        r' `shouldBe` Just ("woot", "1")
+    describe "Header Parsing" $
+      it "can parse a simple header" $ do
+        let m = parseByteString parseHeader mempty headerEx
+            r' = maybeSuccess m
+        print m
+        r' `shouldBe` Just (Header "blah")
+    describe "Comment Parsing" $
+      it "skips comment before header" $ do
+        let p = skipComments >> parseHeader
+            i = "; woot\n[blah]"
+            m = parseByteString p mempty i
+            r' = maybeSuccess m
+        print m
+        r' `shouldBe` Just (Header "blah")
+    describe "Section Parsing" $
+      it "can parse a simple section" $ do
+        let m = parseByteString parseSection mempty sectionEx
+            r' = maybeSuccess m
+            states = M.fromList [("Chris", "Texas")]
+            expected' = Just (Section (Header "states") states)
+        print m
+        r' `shouldBe` expected'
+    describe "INI Parsing" $
+      it "can parse multiple sections" $ do
+        let m = parseByteString parseIni mempty sectionEx''
+            r' = maybeSuccess m
+            sectionValues =
+              M.fromList [("alias", "claw"), ("host", "wikipedia.org")]
+            whatisitValues = M.fromList [("red", "intoothandclaw")]
+            expected' =
+              Just
+                (Config
+                   (M.fromList
+                      [ (Header "section", sectionValues)
+                      , (Header "whatisit", whatisitValues)
+                      ]))
+        print m
+        r' `shouldBe` expected'
