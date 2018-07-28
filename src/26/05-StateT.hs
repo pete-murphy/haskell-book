6fdfec049631d51cbccaf14329d9e29f4306a0ec
Peter Murphy <peterfrancismurphy@gmail.com>
2018-07-27 23:15:07 -0400

    Ch 26.5, p1562:t0?. StateT

---
+++ src/26/05-StateT.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module StateT where
+
+newtype StateT s m a = StateT
+  { runStateT :: s -> m (a, s)
+  }
+
+instance Functor m => Functor (StateT s m) where
+  fmap f (StateT fmas) = StateT $ (fmap . fmap) (\(x, y) -> (f x, y)) fmas
+
+instance Monad m => Applicative (StateT s m) where
+  pure x = StateT $ (\y -> pure (x, y))
+  (<*>) :: StateT s m (a -> b) -> StateT s m a -> StateT s m b
+  StateT f <*> StateT sma =
+    StateT $ \s -> do
+      (g, _) <- f s
+      (x, _) <- sma s
+      return $ (g x, s)
