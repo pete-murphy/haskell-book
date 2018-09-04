fbacbdfe1f12bb96c0b800503a206cae2ac35faf
Peter Murphy <peterfrancismurphy@gmail.com>
2018-09-04 17:37:13 -0400

    Ch 26.9, revisited

--- src/26b/09-Scotty.hs
+++ src/26b/09-Scotty.hs
@@ -3,12 +3,12 @@
 module Scotty where
 
 import           Control.Monad.Trans.Class (lift)
-import           Data.Monoid               (mconcat)
+import           Data.Monoid               ((<>))
 import           Web.Scotty
 
 main =
   scotty 3000 $ do
     get "/:word" $ do
       beam <- param "word"
-      lift $ putStrLn "hello"
-      html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+      lift $ putStrLn $ show beam
+      html $ "<h1>Scotty, " <> beam <> " me up!</h1>"
