4520dd9a6e8afa4b3299d9b5037ba9b88b0aab69
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-25 20:54:25 -0500

    Ch 29.9, Vigenere en/decoder working on command line

--- src/29/9/NewVigenereIO.hs
+++ src/29/9/NewVigenereIO.hs
@@ -2,7 +2,15 @@ module NewVigenereIO where
 
 import           NewVigenere        (unVigenere, vigenere)
 import           System.Environment (getArgs)
-import           System.IO          (hGetChar, hPutChar, hPutStr, stdin, stdout)
+import           System.IO          (hGetChar, hGetLine, hPutChar, hPutStrLn,
+                                     stdin, stdout)
 
 main :: IO ()
-main = hGetChar stdin >>= hPutChar stdout
+main = do
+  [key, opt] <- getArgs
+  str <- hGetLine stdin
+  let out
+        | opt == "-e" = vigenere key str
+        | opt == "-d" = unVigenere key str
+        | otherwise = "Need to provide valid flag"
+  hPutStrLn stdout out
