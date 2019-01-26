5a1136b69c23450366b7479679b2b40f923eb8c0
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-26 10:53:16 -0500

    Added some more safety to NewVigenereIO
    Functionality not right, also would like for encrypt/decrypt to ignore non-letters.

--- src/29/9/NewVigenereIO.hs
+++ src/29/9/NewVigenereIO.hs
@@ -4,12 +4,24 @@ import           NewVigenere        (unVigenere, vigenere)
 import           System.Environment (getArgs)
 import           System.IO          (hGetLine, hPutStrLn, stdin, stdout)
 
+maybeGetArgs :: IO (Maybe [String])
+maybeGetArgs = do
+  args <- getArgs
+  let result =
+        case args of
+          [flag, key] -> Just [flag, key]
+          _           -> Nothing
+  pure result
+
 main :: IO ()
 main = do
-  [opt, key] <- getArgs
+  res <- maybeGetArgs
+  let fn =
+        case res of
+          Nothing -> const "Invalid input"
+          Just [flag, key]
+            | flag == "-d" -> unVigenere key
+            | flag == "-e" -> vigenere key
+            | otherwise -> const "Invalid flag"
   str <- hGetLine stdin
-  let out
-        | opt == "-e" = vigenere key str
-        | opt == "-d" = unVigenere key str
-        | otherwise = "Need to provide valid flag"
-  hPutStrLn stdout out
+  hPutStrLn stdout $ fn str
